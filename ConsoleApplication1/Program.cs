449deabb963d39281d7a93fc6baf9fbba7f65b34
s28128 <s28128@pjwstk.edu.pl>
2024-03-17 17:19:29 +0100

    modyfikacja metoda srednia

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,9 +6,26 @@ namespace ConsoleApplication1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hej 1 modyfikacja");
-            
-            
+            int[] numbers = { 10, 5, 20, 15, 30 };
+            double avarage = FindAvarageValue(numbers);
+            Console.WriteLine("avarage value -> " + avarage);
         }
+
+        public static double FindAvarageValue(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                Console.WriteLine("niepoprawne");
+            }
+
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            return (double)sum / numbers.Length;
+        }
+        
     }
 }
