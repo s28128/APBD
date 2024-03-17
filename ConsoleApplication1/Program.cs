d70625d62b52ae38dac16ade3f35de3c646b6de6
s28128 <s28128@pjwstk.edu.pl>
2024-03-17 16:05:23 +0100

    1 modyfikacja

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+
+namespace ConsoleApplication1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hej 1 modyfikacja");
         }
     }
 }
